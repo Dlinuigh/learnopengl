e8c2816d8eb4cc3b30a99f74075de0eba449666c
lion_claw <lion_claw@163.com>
2024-07-28 23:01:09 +0800

    存在高光位置错误问题

---
+++ assets/glsl/fragment_map.glsl
@@ -0,0 +1,45 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;    
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+    
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;
+in vec2 TexCoord; 
+
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main() {
+    // ambient
+    vec4 ambient = light.ambient * texture(material.diffuse, TexCoord);
+    
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec4 diffuse = light.diffuse * (diff * texture(material.diffuse, TexCoord));
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec4 specular = light.specular * (spec * texture(material.specular, TexCoord));  
+    
+    vec4 result = ambient + diffuse + specular;
+    FragColor = result;
+} 
+
---
+++ lighting/map_effect.cpp
@@ -0,0 +1,137 @@
+#include "material.h"
+void checkError(const char *function) {
+  GLenum err;
+  while ((err = glGetError()) != GL_NO_ERROR) {
+    std::cerr << function << ": " << err << std::endl;
+  }
+}
+void framebuffer_size_callback(GLFWwindow *, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+void Program::process() {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  for (auto &it : children) {
+    it->process();
+  }
+  light_src->process();
+  camera->process();
+}
+void Program::run() {
+  for (auto &it : children) {
+    it->program->link();
+  }
+  light_src->program->link();
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    for (auto &it : children) {
+      it->program->use();
+      it->program->set("view", camera->view);
+      it->program->set("projection", camera->projection);
+      it->activate_textures();
+      it->program->set("material.shininess",64.0f);
+      it->program->set("light.position", light_src->lightPos);
+      it->program->set("light.viewPos", camera->cameraPos);
+      glm::vec4 diffuse = light_src->light_color * glm::vec4(0.5f);
+      glm::vec4 ambient = light_src->light_color * glm::vec4(0.2f);
+      it->program->set("light.ambient", ambient);
+      it->program->set("light.diffuse", diffuse);
+      it->program->set("light.specular", glm::vec4(1.0f));
+      it->set();
+      it->draw();
+    }
+    light_src->program->use();
+    light_src->program->set("view", camera->view);
+    light_src->program->set("projection", camera->projection);
+    light_src->set();
+    light_src->draw();
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+    process();
+  }
+  checkError("run");
+}
+int main() {
+  Program program({800, 600});
+  std::vector<float> vertices = //
+      {
+          -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, //
+          0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f,  //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,   //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,   //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, -1.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, //
+          //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,   //
+          0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,   //
+          -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, //
+
+          -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,   //
+          -0.5f, 0.5f, -0.5f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,  //
+          -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,   //
+          //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,   //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  //
+          0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, //
+          0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, //
+          0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,   //
+          //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, //
+          0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f, -1.0f, 0.0f,  //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,   //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,   //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, //
+          //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   //
+          -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f  //
+      };
+  std::vector<unsigned int> indices = {
+      0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
+      18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+  };
+  std::shared_ptr<Poly> poly =
+      std::make_shared<Poly>(vertices, indices, program.window);
+  std::shared_ptr<ImageTexture> texture1 =
+      std::make_shared<ImageTexture>("assets/img/container2.png");
+  poly->insert("material.diffuse", texture1);
+  std::shared_ptr<ImageTexture> texture2 =
+      std::make_shared<ImageTexture>("assets/img/container2_specular.png");
+  poly->insert("material.specular", texture2);
+  poly->program->load_shader("assets/glsl/vertex_map.glsl", GL_VERTEX_SHADER);
+  poly->program->load_shader("assets/glsl/fragment_map.glsl",
+                             GL_FRAGMENT_SHADER);
+  program.push_back(poly);
+  std::vector<float> light_vectices = {
+      -0.5f, -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,
+      0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,
+      -0.5f, 0.5f,  0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f,
+      0.5f,  -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+      0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  -0.5f,
+      0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,
+      0.5f,  0.5f,  -0.5f, -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, -0.5f,
+      -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,  -0.5f};
+  std::shared_ptr<Light> light =
+      std::make_shared<Light>(light_vectices, indices, program.window);
+  light->program->load_shader("assets/glsl/vertex_color.glsl",
+                              GL_VERTEX_SHADER);
+  light->program->load_shader("assets/glsl/fragment_color_light.glsl",
+                              GL_FRAGMENT_SHADER);
+  program.light_src = std::move(light);
+  program.run();
+}
