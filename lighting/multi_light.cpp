372e41b7f6be7b95c0ee62efffcbcabb414e2772
lion_claw <lion_claw@163.com>
2024-07-29 23:55:19 +0800

    没有更多的测试，直观上功能做完了。

---
+++ lighting/multi_light.cpp
@@ -0,0 +1,194 @@
+#include "multi_light.h"
+// TODO
+// 说一个课程问题，他使用的是glsl代码里面的循环，现在是4个灯，不知道光源环境复杂的情况下会导致什么问题，因为需要考虑不同灯对于同一个物体的效果的累积
+void checkError(const char *function) {
+  GLenum err;
+  while ((err = glGetError()) != GL_NO_ERROR) {
+    std::cerr << function << ": " << err << std::endl;
+  }
+}
+void framebuffer_size_callback(GLFWwindow *, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+void Program::set_light(std::shared_ptr<ShaderProgram> &program) {
+  int i = 0;
+  for (auto &l : light_src) {
+    std::string prefix = "lights[" + std::to_string(i);
+    program->set((prefix + std::string("].position")).c_str(), l->lightPos);
+    program->set((prefix + std::string("].ambient")).c_str(), l->ambient);
+    program->set((prefix + std::string("].diffuse")).c_str(), l->diffuse);
+    program->set((prefix + std::string("].specular")).c_str(), l->specular);
+    program->set((prefix + std::string("].direction")).c_str(),
+                 l->light_type == 0 ? l->direction : camera->cameraFront);
+    program->set((prefix + std::string("].light_type")).c_str(), l->light_type);
+    program->set((prefix + std::string("].constant")).c_str(), l->constant);
+    program->set((prefix + std::string("].linear")).c_str(), l->linear);
+    program->set((prefix + std::string("].quadratic")).c_str(), l->quadratic);
+    program->set((prefix + std::string("].cutOff")).c_str(), l->cutOff);
+    program->set((prefix + std::string("].outerCutoff")).c_str(),
+                 l->outerCutoff);
+    i++;
+  }
+}
+void Program::process() {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  for (auto &it : children) {
+    it->process();
+  }
+  for (auto &l : light_src) {
+    l->process();
+  }
+  camera->process();
+}
+void Program::run() {
+  for (auto &it : children) {
+    it->program->link();
+  }
+  for (auto &l : light_src) {
+    l->program->link();
+  }
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    for (auto &it : children) {
+      it->program->use();
+      it->program->set("view", camera->view);
+      it->program->set("projection", camera->projection);
+      it->activate_textures();
+      it->program->set("material.shininess", 64.0f);
+      set_light(it->program);
+      it->program->set("viewPos", camera->cameraPos);
+      it->set();
+      it->draw();
+    }
+    for (auto &l : light_src) {
+      if (l->light_type != 0) {
+        l->program->use();
+        l->program->set("view", camera->view);
+        l->program->set("projection", camera->projection);
+        l->set();
+        l->draw();
+      }
+    }
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+    process();
+  }
+  checkError("run");
+}
+int main() {
+  Program program({800, 600});
+  std::vector<float> vertices = //
+      {
+          -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, //
+          0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f,  //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,   //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,   //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, -1.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, //
+          //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,   //
+          0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,   //
+          -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,  //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, //
+
+          -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,   //
+          -0.5f, 0.5f, -0.5f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,  //
+          -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f,   //
+          //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,   //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  //
+          0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, //
+          0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, //
+          0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,   //
+          //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, //
+          0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 0.0f, -1.0f, 0.0f,  //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,   //
+          0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, -1.0f, 0.0f,   //
+          -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f,  //
+          -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f, //
+          //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, //
+          0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   //
+          0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,   //
+          -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,  //
+          -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f  //
+      };
+  std::vector<unsigned int> indices = {
+      0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
+      18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+  };
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+  for (unsigned int i = 0; i < 10; i++) {
+    std::shared_ptr<Poly> poly =
+        std::make_shared<Poly>(vertices, indices, program.window);
+    std::shared_ptr<ImageTexture> texture1 =
+        std::make_shared<ImageTexture>("assets/img/container2.png");
+    poly->insert("material.diffuse", texture1);
+    std::shared_ptr<ImageTexture> texture2 =
+        std::make_shared<ImageTexture>("assets/img/container2_specular.png");
+    poly->insert("material.specular", texture2);
+    poly->program->load_shader("assets/glsl/vertex_multi.glsl",
+                               GL_VERTEX_SHADER);
+    poly->program->load_shader("assets/glsl/fragment_multi.glsl",
+                               GL_FRAGMENT_SHADER);
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[i]);
+    float angle = 20.0f * i;
+    model =
+        glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+    poly->model = model;
+    program.push_back(poly);
+  }
+  glm::vec3 pointLightPositions[] = {
+      glm::vec3(0.7f, 0.2f, 2.0f), glm::vec3(2.3f, -3.3f, -4.0f),
+      glm::vec3(-4.0f, 2.0f, -12.0f), glm::vec3(0.0f, 0.0f, -3.0f)};
+  std::vector<float> light_vectices = {
+      -0.5f, -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,
+      0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,
+      -0.5f, 0.5f,  0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f,
+      0.5f,  -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+      0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  0.5f,  -0.5f,
+      0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,
+      0.5f,  0.5f,  -0.5f, -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  -0.5f, -0.5f, 0.5f,  -0.5f, -0.5f, -0.5f,
+      -0.5f, 0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  0.5f,  0.5f,
+      0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,  -0.5f};
+  for (auto &p : pointLightPositions) {
+    std::shared_ptr<Light> light =
+        std::make_shared<Light>(light_vectices, indices, program.window);
+    light->program->load_shader("assets/glsl/vertex_color.glsl",
+                                GL_VERTEX_SHADER);
+    light->program->load_shader("assets/glsl/fragment_color_light.glsl",
+                                GL_FRAGMENT_SHADER);
+    light->direction = glm::vec3(-0.2f, -1.0f, -0.3f);
+    light->diffuse = glm::vec4(0.8f);
+    light->ambient = glm::vec4(0.1f);
+    light->specular = glm::vec4(1.0f);
+    light->constant = 1.0f;
+    light->linear = 0.09f;
+    light->quadratic = 0.032f;
+    light->cutOff = glm::cos(glm::radians(12.5f));
+    light->outerCutoff = glm::cos(glm::radians(17.5f));
+    light->lightPos = p;
+    light->light_type = rand()%3;
+    program.light_src.push_back(std::move(light));
+  }
+  program.run();
+}
---
+++ lighting/multi_light.h
@@ -0,0 +1,411 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <memory>
+#include <vector>
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+void checkError(const char *function);
+class ShaderProgram {
+  unsigned int id;
+  void check(void fun(unsigned int, unsigned int, int *), unsigned int sid,
+             unsigned int flag, const char *message,
+             void log(unsigned int, int, int *, char *)) {
+    int success;
+    fun(sid, flag, &success);
+    char info[512];
+    if (!success) {
+      log(sid, 512, NULL, info);
+      std::cerr << message << ":\n  " << info << "\n";
+    }
+  }
+
+public:
+  ShaderProgram() { id = glCreateProgram(); }
+  ~ShaderProgram() { glDeleteProgram(id); }
+  void load_shader(const char *filename, int flag) {
+    std::ifstream stream(filename, std::ios::in);
+    if (stream.is_open()) {
+      std::string tmp((std::istreambuf_iterator(stream)),
+                      std::istreambuf_iterator<char>());
+      const char *src = tmp.c_str();
+      unsigned int sid = glCreateShader(flag);
+      glShaderSource(sid, 1, &src, NULL);
+      glCompileShader(sid);
+      check(glGetShaderiv, sid, GL_COMPILE_STATUS, "Shader",
+            glGetShaderInfoLog);
+      glAttachShader(id, sid);
+      glDeleteShader(sid);
+      stream.close();
+    }
+  }
+  void link() {
+    glLinkProgram(id);
+    check(glGetProgramiv, id, GL_LINK_STATUS, "Shader Program",
+          glGetProgramInfoLog);
+  }
+  void use() { glUseProgram(id); }
+  void set(const char* name, glm::ivec4 value) const {
+    glUniform4i(glGetUniformLocation(id, name), value.x, value.y,
+                value.z, value.w);
+  }
+  void set(const char* name, glm::fvec4 value) const {
+    glUniform4f(glGetUniformLocation(id, name), value.x, value.y,
+                value.z, value.w);
+  }
+  void set(const char* name, glm::fvec3 value) const {
+    glUniform3f(glGetUniformLocation(id, name), value.x, value.y,
+                value.z);
+  }
+  void set(const char* name, bool value) const {
+    glUniform1i(glGetUniformLocation(id, name), value);
+  }
+  void set(const char* name, int value) const {
+    glUniform1i(glGetUniformLocation(id, name), value);
+  }
+  void set(const char* name, float value) const {
+    glUniform1f(glGetUniformLocation(id, name), value);
+  }
+  void set(const char* name, glm::mat4 &value) const {
+    glUniformMatrix4fv(glGetUniformLocation(id, name), 1, GL_FALSE,
+                       glm::value_ptr(value));
+  }
+};
+class ImageTexture {
+  unsigned int id;
+
+public:
+  int index;
+  ImageTexture(const char *filename) {
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height, nrChannels;
+    // stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(filename, &width, &height, &nrChannels, 0);
+    if (data) {
+      int mode = nrChannels == 3 ? GL_RGB : GL_RGBA;
+      glTexImage2D(GL_TEXTURE_2D, 0, mode, width, height, 0, mode,
+                   GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+  }
+  void activate(int idx) {
+    glActiveTexture(GL_TEXTURE0 + idx);
+    glBindTexture(GL_TEXTURE_2D, id);
+    index = idx;
+  }
+  ~ImageTexture() { glDeleteTextures(1, &id); }
+};
+class Poly {
+  std::vector<float> vertices;
+  std::vector<unsigned int> indices;
+  unsigned int vao;
+  GLFWwindow *window;
+
+public:
+  glm::mat4 model;
+  std::map<std::string, std::shared_ptr<ImageTexture>> textures;
+  std::shared_ptr<ShaderProgram> program = std::make_shared<ShaderProgram>();
+  Poly(std::vector<float> _vertices, std::vector<unsigned int> _indices,
+       GLFWwindow *_window)
+      : vertices(std::move(_vertices)), indices(std::move(_indices)),
+        window(_window) {
+    unsigned int vbo, ebo;
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(),
+                 vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(),
+                 indices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void *)(5 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    model = glm::mat4(1.0f);
+  }
+  ~Poly() {
+    for (auto &texture : textures) {
+      texture.second.reset();
+    }
+    program.reset();
+    glDeleteVertexArrays(1, &vao);
+  }
+  void insert(const char* name, std::shared_ptr<ImageTexture> &texture) {
+    textures[name] = std::move(texture);
+  }
+  void activate_textures() {
+    if (!textures.empty()) {
+      int i = 0;
+      for (auto &t : textures) {
+        t.second->activate(i);
+        program->set(t.first.c_str(), i);
+        i++;
+      }
+    }
+  }
+  void set() { program->set("model", model); }
+  void process() {}
+  void draw() {
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+  }
+};
+void framebuffer_size_callback(GLFWwindow *, int width, int height);
+class Camera;
+class Light {
+  GLFWwindow *window;
+  std::vector<float> vertices;
+  std::vector<unsigned int> indices;
+  unsigned int vao;
+
+public:
+  glm::vec3 lightPos;
+  glm::vec4 light_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+  glm::vec4 ambient;
+  glm::vec4 diffuse;
+  glm::vec4 specular;
+  glm::vec3 direction;
+  glm::mat4 model;
+  float constant;
+  float linear;
+  float quadratic;
+  float cutOff;
+  float outerCutoff;
+  int light_type = 1;
+  std::shared_ptr<ShaderProgram> program = std::make_shared<ShaderProgram>();
+  Light(std::vector<float> _vertices, std::vector<unsigned int> _indices,
+        GLFWwindow *_window)
+      : window(_window), vertices(std::move(_vertices)),
+        indices(std::move(_indices)) {
+    unsigned int vbo, ebo;
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(),
+                 vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(),
+                 indices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+  }
+  ~Light() {
+    program.reset();
+    glDeleteVertexArrays(1, &vao);
+  }
+  void process() {
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+      light_color.x -= light_color.x > 0.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+      light_color.x += light_color.x < 1.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+      light_color.y -= light_color.y > 0.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+      light_color.y += light_color.y < 1.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+      light_color.z -= light_color.z > 0.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+      light_color.z += light_color.z < 1.0f ? 0.01f : 0.0f;
+    if (glfwGetKey(window, GLFW_KEY_COMMA) == GLFW_PRESS) {
+      lightPos.z -= 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_PERIOD) == GLFW_PRESS) {
+      lightPos.z += 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
+      lightPos.y -= 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_SEMICOLON) == GLFW_PRESS) {
+      lightPos.y += 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+      lightPos.x -= 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_BRACKET) == GLFW_PRESS) {
+      lightPos.x += 0.1f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+      light_type = 1;
+    }
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+      light_type = 2;
+    }
+    if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS) {
+      light_type = 0;
+    }
+  }
+  void set() {
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, lightPos);
+    model = glm::scale(model, glm::vec3(0.2f));
+    program->set("model", model);
+    program->set("light_color", light_color);
+  }
+  void draw() {
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+  }
+};
+class Program {
+  float scale = 0.5f;
+  glm::ivec2 scr_size = {};
+  std::shared_ptr<Camera> camera = nullptr;
+  void glad_init() {
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (!version) {
+      std::cout << "Failed to initialize GLAD\n" << version << std::endl;
+      exit(-1);
+    } else {
+      printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version),
+             GLAD_VERSION_MINOR(version));
+    }
+  }
+  std::vector<std::shared_ptr<Poly>> children;
+
+public:
+  GLFWwindow *window;
+  // std::shared_ptr<Light> light_src = nullptr;
+  std::vector<std::shared_ptr<Light>> light_src;
+  Program(glm::ivec2 _size) : scr_size(_size) {
+    glfwInit();
+    window = glfwCreateWindow(scr_size.x, scr_size.y, "Hello Window", nullptr,
+                              nullptr);
+    glfwMakeContextCurrent(window);
+    glad_init();
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, scr_size.x, scr_size.y);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    camera = std::make_shared<Camera>(window, scr_size);
+  }
+  ~Program() {
+    for (auto &child : children) {
+      child.reset();
+    }
+    for (auto &l : light_src) {
+      l.reset();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+  void push_back(std::shared_ptr<Poly> &poly) { children.push_back(poly); }
+  void push_back(std::shared_ptr<Light> &light) { light_src.push_back(light); }
+  void set_light(std::shared_ptr<ShaderProgram> &program);
+  void process();
+  void run();
+};
+class Camera {
+  bool firstMouse = true;
+  float yaw = -90.0f, pitch = 0.0f, fov = 45.0f;
+  double lastX, lastY;
+  glm::ivec2 scr_size;
+  GLFWwindow *window;
+  glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+  const float cameraSpeed = 0.05f;
+  static void mouse_callback_handler(GLFWwindow *window, double xpos,
+                                     double ypos) {
+    Camera *camera = static_cast<Camera *>(glfwGetWindowUserPointer(window));
+    if (camera)
+      camera->mouse_callback(window, xpos, ypos);
+  }
+  static void scroll_callback_handler(GLFWwindow *window, double xoffset,
+                                      double yoffset) {
+    Camera *camera = static_cast<Camera *>(glfwGetWindowUserPointer(window));
+    if (camera)
+      camera->scroll_callback(window, xoffset, yoffset);
+  }
+  void mouse_callback(GLFWwindow *, double xpos, double ypos) {
+    if (firstMouse) {
+      lastX = xpos;
+      lastY = ypos;
+      firstMouse = false;
+    }
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+    yaw += xoffset;
+    pitch += yoffset;
+    if (pitch > 89.0f)
+      pitch = 89.0f;
+    if (pitch < -89.0f)
+      pitch = -89.0f;
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+  }
+  void scroll_callback(GLFWwindow *, double, double yoffset) {
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+      fov = 1.0f;
+    if (fov > 45.0f)
+      fov = 45.0f;
+  }
+
+public:
+  glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+  glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
+  glm::mat4 view = glm::mat4(1.0f), projection = glm::mat4(1.0f);
+  Camera(GLFWwindow *_window, glm::ivec2 _scr_size)
+      : scr_size(_scr_size), window(_window) {
+    glfwSetWindowUserPointer(_window, this);
+    glfwSetCursorPosCallback(_window, mouse_callback_handler);
+    glfwSetScrollCallback(_window, scroll_callback_handler);
+    lastX = scr_size.x / 2.0;
+    lastY = scr_size.y / 2.0;
+  }
+  void process() {
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+      cameraPos -=
+          glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+      cameraPos +=
+          glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    projection = glm::perspective(glm::radians(fov),
+                                  (float)scr_size.x / scr_size.y, 0.1f, 100.0f);
+  }
+};
+#endif
